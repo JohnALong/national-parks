20996e3c04222fa98f6a47af7315edc549fff2d3
John Long <jlcomp45@gmail.com>
2019-10-25 15:20:30 -0500

    mvp met - added conditional creating classes to dom to add borders, correccted data requested in assignment to park and state as well as dom structure to inclue article tag, h3, and p tag

--- src/scripts/domComponent.js
+++ src/scripts/domComponent.js
@@ -1,8 +1,14 @@
 const parkToDom = {
     parkDataComponent: (parkInfo) => {
         console.log("domTest", parkInfo.name);
+        let parkVisited = ""
+        if (parkInfo.visited === true) {
+            parkVisited = "visited";
+        } else {
+            parkVisited = "notVisited"
+        }
         return `
-        <article>
+        <article class="${parkVisited}">
             <h3 class="parkName">${parkInfo.name}</h3>
             <div class="parkState">${parkInfo.state}</div>
         </article>    
