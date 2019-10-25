ff24d6fbc4019cea8783d947bc686f6207447552
John Long <jlcomp45@gmail.com>
2019-10-25 14:52:14 -0500

    called api, converted to usable data, populated dom, linked style sheet

--- src/scripts/domInfo.js
+++ src/scripts/domInfo.js
@@ -1,1 +1,14 @@
-console.log("hi");
+const allParks = {
+    parkBreakDown: (parkArray) => {
+        let htmlString = "";
+        console.log("testdata", parkArray);
+        parkArray.forEach(entry => {
+            console.log("test test");
+            htmlString += parkToDom.parkDataComponent(entry);
+        })
+        console.log("test 2");
+        const parkDom = document.querySelector(".container");
+        console.log("testdata2", parkDom);
+        parkDom.innerHTML = htmlString;
+    }
+}
